f894d223a2b3518dd3814f245fb4bd5fc2f051e2
juyeongkyeong <juyk.oung@gmail.com>
2026-01-14 22:23:38 +0900

    feat: 펀딩 생성을 위한 이벤트 구현
    임시로 구현한 Payment, WishList 이벤트 익일 삭제 예정

---
+++ bc/funding/src/main/java/in/FundingEventListener.java
@@ -0,0 +1,28 @@
+package in;
+
+import app.funding.FundingSyncItemUseCase;
+import in.event.WishlistItemCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMIT;
+
+@Component
+@RequiredArgsConstructor
+public class FundingEventListener {
+    
+    private final FundingSyncItemUseCase fundingSyncItemUseCase;
+    
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void handleWishlistItemCreated(WishlistItemCreatedEvent event) {
+        fundingSyncItemUseCase.syncItem(
+            event.getWishlistItemId(),
+            event.getProductId()
+        );
+    }
+}
+
---
+++ bc/funding/src/main/java/in/PaymentEventListener.java
@@ -0,0 +1,28 @@
+package in;
+
+import app.funding.FundingFacade;
+import in.event.PaymentCompletedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMIT;
+
+@Component
+@RequiredArgsConstructor
+public class PaymentEventListener {
+    
+    private final FundingFacade fundingFacade;
+    
+    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void handlePaymentCompleted(PaymentCompletedEvent event) {
+        fundingFacade.createFunding(
+            event.getPayerId(),
+            event.getWishlistItemId(),
+            event.getAmount()
+        );
+    }
+}
