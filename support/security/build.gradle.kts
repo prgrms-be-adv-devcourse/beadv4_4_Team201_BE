dfa7a257f8f1c43822facdb034647261012fc0b3
chan99k <kjkj5868@gmail.com>
2026-01-14 09:17:47 +0900

    Chore: 사용하지 않는 buildSrc 삭제 및 Gradle 모듈 구조 추가 및 설정

---
+++ support/security/build.gradle.kts
@@ -0,0 +1,7 @@
+// Support Security - 보안 유틸리티
+
+dependencies {
+    implementation(project(":support:common"))
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+}
