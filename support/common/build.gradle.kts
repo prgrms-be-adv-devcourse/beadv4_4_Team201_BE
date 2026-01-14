37550bdd3d9249f2bffda8610f04be7e60001d48
juyeongkyeong <juyk.oung@gmail.com>
2026-01-14 22:09:17 +0900

    feat: BaseEntity 구현 및 설정 적용

--- support/common/build.gradle.kts
+++ support/common/build.gradle.kts
@@ -3,4 +3,8 @@
 dependencies {
     // Minimal Spring
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("jakarta.persistence:jakarta.persistence-api")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 }
