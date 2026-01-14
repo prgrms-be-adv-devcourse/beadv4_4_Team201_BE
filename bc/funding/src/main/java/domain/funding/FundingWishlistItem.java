82d0527b255f671f60b7b63f88a5708e9a895e54
juyeongkyeong <juyk.oung@gmail.com>
2026-01-14 22:16:59 +0900

    feat: Funding 도메인에서 사용할 위시리스트 아이템 엔티티 및 동기화 구현

---
+++ bc/funding/src/main/java/domain/funding/FundingWishlistItem.java
@@ -0,0 +1,41 @@
+package domain.funding;
+
+import domain.Product;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import jpa.BaseEntity;
+
+@Entity
+@Table(name = "FUNDING_WISHLIST_ITEM")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FundingWishlistItem extends BaseEntity {
+
+    public enum WishListItemStatus {
+        PENDING,                // 위시리스트에 담긴 상태
+        IN_PROGRESS,            // 펀딩 진행 중
+        REQUESTED_CONFIRM,      // 수령자 확정 대기 중
+        COMPLETED               // 수령자 확정 완료
+    }
+
+    @Column(nullable = false)
+    private Long wishlistId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private WishListItemStatus status;
+
+
+    public FundingWishlistItem(Long wishlistId, Product product, WishListItemStatus status) {
+        this.wishlistId = wishlistId;
+        this.product = product;
+        this.status = status;
+    }
+
+}
