880582a416022ae0a7759a5c0abefed49fb391fe
chan99k <kjkj5868@gmail.com>
2026-01-14 22:59:56 +0900

    refactor:  `Money` VO 패키지 구조 수정

--- bc/money/core/src/test/java/domain/payment/PaymentTest.java
+++ bc/money/core/src/test/java/domain/payment/PaymentTest.java
@@ -2,7 +2,7 @@ package domain.payment;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import vo.Money;
+import app.giftify.shared.domain.vo.Money;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
