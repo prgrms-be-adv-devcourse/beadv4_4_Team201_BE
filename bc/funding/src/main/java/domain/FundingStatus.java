4add5c53b6fa4031c6866916fe80165d013c7a3d
juyeongkyeong <juyk.oung@gmail.com>
2026-01-14 22:07:23 +0900

    삭제파일

--- bc/funding/src/main/java/domain/FundingStatus.java
+++
@@ -1,8 +0,0 @@
-package domain;
-
-public enum FundingStatus {
-    IN_PROGRESS,    // 펀딩 진행 중
-    ACHIEVED,       // 목표액 달성
-    EXPIRED,        // 기한 만료
-    CLOSED          // 시스템에 의한 펀딩 종료 OR 수령자가 2주 내 확정 누르지 않아 종료된 상태
-}
