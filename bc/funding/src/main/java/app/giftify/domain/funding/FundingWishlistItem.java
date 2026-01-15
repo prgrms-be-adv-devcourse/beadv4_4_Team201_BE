e99475d15728568c612bba8acc620fc384b7e063
Suyeoneee <101303761+suyeon1717@users.noreply.github.com>
2026-01-15 21:30:23 +0900

    펀딩 모듈 패키지 구조 변경 (#67)
    ## Summary
    
    > 이 `PR`에서 `제공`하는 `솔루션`과 그 `설계 의도`를 설명해 주세요. _( like 흑백요리사 )_
    > `변경사항`이 필요한 이유, 즉 `컨텍스트` 와 해결하려는 `문제의 핵심`을 `요약`하고, `리뷰어`가 `어떤 관점`에서
    봐줬으면 좋겠는지 명시해주세요.
    
    펀딩 모듈 패키지 구조를 변경합니다. 
    - 패키지는 펀딩 모듈 안에서 app.giftify. 으로 시작합니다. 
    ```plain text
    package app.product
    -> package app.giftify.app.product
    
    package domain.product
    -> package app.giftify.domain.product
    ```
    
    ---
    
    ## What's Changed
    
    > `PR`에서 `변경되는 점`들을 설명해 주세요. `UI 변경`이 있다면 `스크린샷`을 첨부해주세요.
    
    - app.giftify. ~ 으로 펀딩 패키지 구조를 수정함으로써 펀딩 모듈 하위 클래스들도 스캔이 됩니다. 
    - 펀딩 모듈이 스캔 대상이 되면서 Querydsl config 클래스를 추가하였습니다. 
    
    ---
    
    ## Decisions & Trade-offs
    
    > 개발중에 추가로 고려한 부분에 대해서 공유해주세요. 예를 들어,<br>
    > `A 방식 대신 B 방식을 선택한 이유`나, `성능` 혹은 `사용자 경험`을 위해 고민한 흔적을 남겨주세요.
    
    - Querydsl config 클래스를 어디에 위치시켜야 할 지 고민되어 노션 이슈풀에 적어 놓았습니다.
    
    ---
    
    ## Future Improvements
    
    > 이번 `PR`의 `범위`에는 포함되지 않았지만, 추후 `고도화`하거나 `보완`하고 싶은 점이 있다면 적어주세요.
    > 예를 들어, 남겨둔 `기술 부채`나 `최적화` 계획, 혹은 `확장성`을 위해 다음 단계에서 `시도해 볼 만한 아이디어`를
    공유해주세요.
    
    
    ---
    
    ### Linked Issues
    
    - closes #

---
+++ bc/funding/src/main/java/app/giftify/domain/funding/FundingWishlistItem.java
@@ -0,0 +1,47 @@
+package app.giftify.domain.funding;
+
+import app.giftify.domain.product.Product;
+import app.giftify.support.jpa.BaseJpaEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "FUNDING_WISHLIST_ITEM")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FundingWishlistItem extends BaseJpaEntity {
+
+	public enum WishListItemStatus {
+		PENDING,                // 위시리스트에 담긴 상태
+		IN_PROGRESS,            // 펀딩 진행 중
+		REQUESTED_CONFIRM,      // 수령자 확정 대기 중
+		COMPLETED               // 수령자 확정 완료
+	}
+
+	@Column(nullable = false)
+	private Long wishlistId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id", nullable = false)
+	private Product product;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private WishListItemStatus status;
+
+	public FundingWishlistItem(Long wishlistId, Product product, WishListItemStatus status) {
+		this.wishlistId = wishlistId;
+		this.product = product;
+		this.status = status;
+	}
+
+}
---
+++ bc/funding/src/main/java/app/giftify/domain/product/Product.java
@@ -0,0 +1,59 @@
+package app.giftify.domain.product;
+
+import static jakarta.persistence.GenerationType.*;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import app.giftify.domain.FundingMember;
+import app.giftify.in.product.ProductDto;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "PRODUCT")
+@NoArgsConstructor
+@Getter
+public class Product { //todo validation
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+	@ManyToOne
+	@JoinColumn(name = "member_id")
+	private FundingMember seller;
+	private String name;
+	private String description;
+	private int price;
+	private int stock;
+	private ProductStatus status;
+
+	@CreatedDate
+	private LocalDateTime createdAt;
+	@LastModifiedDate
+	private LocalDateTime modifiedAt;
+
+	public Product(FundingMember seller, String name, String description, int price, int stock) {
+		this.seller = seller;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.stock = stock;
+		this.status = ProductStatus.DRAFT;
+	}
+
+	public ProductDto toDto() {
+		return new ProductDto(getId(), getSeller().getNickname(), getName(), getDescription(), getPrice(), getStock());
+	}
+
+	public void approveProduct() {
+		this.status = ProductStatus.ACTIVE;
+	}
+}
