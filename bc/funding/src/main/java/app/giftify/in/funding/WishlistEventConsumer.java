85edb35c75d44d0b8e8f07dbf6870772023f90e1
juyeongkyeong <juyk.oung@gmail.com>
2026-01-16 00:26:21 +0900

    refactor: 진입점을 facade로 통일

--- bc/funding/src/main/java/app/giftify/in/funding/WishlistEventConsumer.java
+++ bc/funding/src/main/java/app/giftify/in/funding/WishlistEventConsumer.java
@@ -1,6 +1,6 @@
 package app.giftify.in.funding;
 
-import app.giftify.app.funding.FundingSyncItemUseCase;
+import app.giftify.app.funding.FundingFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -8,18 +8,18 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class WishlistEventConsumer {
 
-    private final FundingSyncItemUseCase fundingSyncItemUseCase;
+    private final FundingFacade fundingFacade;
 
     // TODO: Member BC에 WishlistItemCreatedEvent 구현 후 주석 해제
-    /*
-    @TransactionalEventListener(phase = AFTER_COMMIT)
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void handleWishlistItemCreated(WishlistItemCreatedEvent event) {
-        fundingSyncItemUseCase.syncItem(
-            event.getWishlistItemId(),
-            event.getProductId()
-        );
-    }
-    */
+
+//    @TransactionalEventListener(phase = AFTER_COMMIT)
+//    @Transactional(propagation = Propagation.REQUIRES_NEW)
+//    public void handleWishlistItemCreated(WishlistItemCreatedEvent event) {
+//        fundingFacade.syncItem(
+//            event.getWishlistItemId(),
+//            event.getProductId()
+//        );
+//    }
+
 }
 
