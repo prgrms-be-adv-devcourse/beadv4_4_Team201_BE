a4714c2cef4e2702c4e6682756aa499ea1ffbcbc
youngjuKim <kjuzoojuk@naver.com>
2026-01-15 11:48:06 +0900

    refactor(auth): 인증 성공 이벤트를 공유 모듈(shared)로 이전
    - UserAuthenticatedEvent를 bc/auth에서 bc/shared로 이동
    - 모든 서비스 모듈에서 공통 이벤트를 구독할 수 있는 구조 확립
    - bc/auth 모듈의 서비스 및 리스너가 공유 이벤트를 참조하도록 리팩토링

--- bc/shared/build.gradle.kts
+++ bc/shared/build.gradle.kts
@@ -1,2 +1,8 @@
   // BC Shared Kernel - 모든 BC 모듈이 공유하는 도메인 개념
   // 순수 Java, 외부 의존성 최소화
+
+  dependencies {
+      implementation("org.springframework.boot:spring-boot-starter")
+      compileOnly("org.projectlombok:lombok")
+      annotationProcessor("org.projectlombok:lombok")
+  }
