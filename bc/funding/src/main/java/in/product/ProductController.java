0733d085a213ef7c07b34994244c5dd74b2b72a2
Suyeoneee <101303761+suyeon1717@users.noreply.github.com>
2026-01-15 14:49:36 +0900

    버전 정리, 상품 검색 파라미터를 dto로 관리 (#60)
    ## Summary
    
    > 이 `PR`에서 `제공`하는 `솔루션`과 그 `설계 의도`를 설명해 주세요. _( like 흑백요리사 )_
    > `변경사항`이 필요한 이유, 즉 `컨텍스트` 와 해결하려는 `문제의 핵심`을 `요약`하고, `리뷰어`가 `어떤 관점`에서
    봐줬으면 좋겠는지 명시해주세요.
    
    PR(#54) 피드백 반영하여 수정했습니다.
    
    
    1. 버전 정리
    - querydsl 버전을 libs.versions.toml 에 정리
    ```toml
    # Database
    mysql = "8.3.0"
    h2 = "2.2.224"
    querydsl = "5.0.0" # query builder
    
    # --- Database ---
    mysql-connector = { module = "com.mysql:mysql-connector-j", version.ref = "mysql" }
    h2 = { module = "com.h2database:h2", version.ref = "h2" }
    querydsl-jpa = { module = "com.querydsl:querydsl-jpa", version.ref = "querydsl" } # 런타임 lib
    querydsl-apt = { module = "com.querydsl:querydsl-apt", version.ref = "querydsl" } # 컴파일 lib
    ```
    2. 상품 검색 파라미터를 dto로 관리
    - 검색 파리미터를 ProductSearchDto에 묶어 관리
    - Controller에서 @ModelAttribute 적용
    ```java
    @GetMapping("/search")
    	public ResponseEntity<Page<ProductDto>> search(
    		@ModelAttribute ProductSearchDto searchDto
    	) {
    		Page<ProductDto> searchResult = productFacade.search(searchDto);
    
    		return ResponseEntity.status(OK).body(searchResult);
    	}
    ```
    ---
    
    ## What's Changed
    
    > `PR`에서 `변경되는 점`들을 설명해 주세요. `UI 변경`이 있다면 `스크린샷`을 첨부해주세요.
    
    
    ---
    
    ## Decisions & Trade-offs
    
    > 개발중에 추가로 고려한 부분에 대해서 공유해주세요. 예를 들어,<br>
    > `A 방식 대신 B 방식을 선택한 이유`나, `성능` 혹은 `사용자 경험`을 위해 고민한 흔적을 남겨주세요.
    
    
    ---
    
    ## Future Improvements
    
    > 이번 `PR`의 `범위`에는 포함되지 않았지만, 추후 `고도화`하거나 `보완`하고 싶은 점이 있다면 적어주세요.
    > 예를 들어, 남겨둔 `기술 부채`나 `최적화` 계획, 혹은 `확장성`을 위해 다음 단계에서 `시도해 볼 만한 아이디어`를
    공유해주세요.
    
    
    ---
    
    ### Linked Issues
    
    - Resolves #45 (sub: #51 #52)

--- bc/funding/src/main/java/in/product/ProductController.java
+++ bc/funding/src/main/java/in/product/ProductController.java
@@ -7,12 +7,12 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.product.ProductFacade;
@@ -63,16 +63,9 @@ public class ProductController {
 	// todo 엘라스틱서치
 	@GetMapping("/search")
 	public ResponseEntity<Page<ProductDto>> search(
-		@RequestParam(required = false) String keyword,
-		@RequestParam(required = false) Integer minPrice,
-		@RequestParam(required = false) int maxPrice,
-		@RequestParam(required = false) boolean inStock,
-		@RequestParam(required = false, defaultValue = "latest") String sort,
-		@RequestParam(defaultValue = "0") int page,
-		@RequestParam(defaultValue = "20") int size
+		@ModelAttribute ProductSearchDto searchDto
 	) {
-		var searchQuery = new ProductSearchDto(keyword, minPrice, maxPrice, inStock, sort, page, size);
-		Page<ProductDto> searchResult = productFacade.search(searchQuery);
+		Page<ProductDto> searchResult = productFacade.search(searchDto);
 
 		return ResponseEntity.status(OK).body(searchResult);
 	}
