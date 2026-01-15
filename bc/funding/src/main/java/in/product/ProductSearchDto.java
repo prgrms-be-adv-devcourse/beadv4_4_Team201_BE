2c25bb158d70e5dbbec95edc4500b55648b2c779
Suyeoneee <101303761+suyeon1717@users.noreply.github.com>
2026-01-15 14:49:36 +0900

    버전 정리, 상품 검색 파라미터를 dto로 관리 (#60)
    ## Summary
    
    > 이 `PR`에서 `제공`하는 `솔루션`과 그 `설계 의도`를 설명해 주세요. _( like 흑백요리사 )_
    > `변경사항`이 필요한 이유, 즉 `컨텍스트` 와 해결하려는 `문제의 핵심`을 `요약`하고, `리뷰어`가 `어떤 관점`에서
    봐줬으면 좋겠는지 명시해주세요.
    
    PR(#54) 피드백 반영하여 수정했습니다.
    
    
    1. 버전 정리
    - querydsl 버전을 libs.versions.toml 에 정리
    ```toml
    # Database
    mysql = "8.3.0"
    h2 = "2.2.224"
    querydsl = "5.0.0" # query builder
    
    # --- Database ---
    mysql-connector = { module = "com.mysql:mysql-connector-j", version.ref = "mysql" }
    h2 = { module = "com.h2database:h2", version.ref = "h2" }
    querydsl-jpa = { module = "com.querydsl:querydsl-jpa", version.ref = "querydsl" } # 런타임 lib
    querydsl-apt = { module = "com.querydsl:querydsl-apt", version.ref = "querydsl" } # 컴파일 lib
    ```
    2. 상품 검색 파라미터를 dto로 관리
    - 검색 파리미터를 ProductSearchDto에 묶어 관리
    - Controller에서 @ModelAttribute 적용
    ```java
    @GetMapping("/search")
    	public ResponseEntity<Page<ProductDto>> search(
    		@ModelAttribute ProductSearchDto searchDto
    	) {
    		Page<ProductDto> searchResult = productFacade.search(searchDto);
    
    		return ResponseEntity.status(OK).body(searchResult);
    	}
    ```
    ---
    
    ## What's Changed
    
    > `PR`에서 `변경되는 점`들을 설명해 주세요. `UI 변경`이 있다면 `스크린샷`을 첨부해주세요.
    
    
    ---
    
    ## Decisions & Trade-offs
    
    > 개발중에 추가로 고려한 부분에 대해서 공유해주세요. 예를 들어,<br>
    > `A 방식 대신 B 방식을 선택한 이유`나, `성능` 혹은 `사용자 경험`을 위해 고민한 흔적을 남겨주세요.
    
    
    ---
    
    ## Future Improvements
    
    > 이번 `PR`의 `범위`에는 포함되지 않았지만, 추후 `고도화`하거나 `보완`하고 싶은 점이 있다면 적어주세요.
    > 예를 들어, 남겨둔 `기술 부채`나 `최적화` 계획, 혹은 `확장성`을 위해 다음 단계에서 `시도해 볼 만한 아이디어`를
    공유해주세요.
    
    
    ---
    
    ### Linked Issues
    
    - Resolves #45 (sub: #51 #52)

--- bc/funding/src/main/java/in/product/ProductSearchDto.java
+++ bc/funding/src/main/java/in/product/ProductSearchDto.java
@@ -1,12 +1,20 @@
 package in.product;
 
-public record ProductSearchDto(
-	String keyword,
-	Integer minPrice,
-	Integer maxPrice,
-	Boolean inStock,
-	String sort,
-	int page,
-	int size
-) {
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProductSearchDto {
+	@NotNull
+	private String keyword;
+	private Integer minPrice;
+	private Integer maxPrice;
+	private Boolean inStock = false;
+	private String sort = "latest";
+	private int page = 0;
+	private int size = 20;
 }
