7fd783ff7e01a6d723a8c78515837ddecc3734e8
seongeun308 <kse011010@gmail.com>
2026-01-16 16:41:44 +0900

    feat: 지갑 출금 완료 이벤트 추가
    - WalletWithdrawnEvent 도메인 이벤트 생성
    - 출금 관련 데이터(잔액, 거래 유형, 참조 정보 등) 관리 목적
    - 기타 관련 주석 추가 및 개선

--- bc/shared/src/main/java/app/giftify/shared/domain/event/wallet/WalletWithdrawnEvent.java
+++ bc/shared/src/main/java/app/giftify/shared/domain/event/wallet/WalletWithdrawnEvent.java
@@ -3,8 +3,8 @@ package app.giftify.shared.domain.event.wallet;
 import app.giftify.shared.domain.event.BaseDomainEvent;
 import app.giftify.shared.domain.vo.Money;
 
-// todo: WalletChargedEvent로 rename
-public class WalletChargeCompletedEvent extends BaseDomainEvent {
+// todo: 하나만 WalletEvent에 EventType을 통해 이벤트를 구분? 아님 각 개별 이벤트 생성?
+public class WalletWithdrawnEvent extends BaseDomainEvent {
     private final Long walletId;
     private final String transactionType;
     private final Money amount;
@@ -12,7 +12,7 @@ public class WalletChargeCompletedEvent extends BaseDomainEvent {
     private final String referenceType;
     private final Long referenceId;
 
-    public WalletChargeCompletedEvent(Long walletId, String transactionType, Money amount, Money balanceAfter, String referenceType, Long referenceId) {
+    public WalletWithdrawnEvent(Long walletId, String transactionType, Money amount, Money balanceAfter, String referenceType, Long referenceId) {
         this.walletId = walletId;
         this.transactionType = transactionType;
         this.amount = amount;
