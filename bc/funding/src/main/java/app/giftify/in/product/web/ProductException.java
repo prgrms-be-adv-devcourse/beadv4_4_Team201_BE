b1990c44cee088522271c9b6b23ad235289f0fb6
Suyeoneee <101303761+suyeon1717@users.noreply.github.com>
2026-01-16 16:43:59 +0900

    Delete in/product/web/ProductException.java

--- bc/funding/src/main/java/app/giftify/in/product/web/ProductException.java
+++
@@ -1,9 +0,0 @@
-package app.giftify.in.product.web;
-
-import app.giftify.shared.api.exception.BusinessException;
-
-public class ProductException extends BusinessException {
-	public ProductException(ProductErrorCode errorCode) {
-		super(errorCode);
-	}
-}
