7187b4fdafba198507927525c4ef6fd31130d316
chan99k <kjkj5868@gmail.com>
2026-01-20 23:36:28 +0900

    fix: PaymentType enum 폴더 구조 개선 필요성 주석 추가 및 FIXME 표시

--- bc/shared/src/main/java/app/giftify/shared/domain/event/payment/PaymentType.java
+++ bc/shared/src/main/java/app/giftify/shared/domain/event/payment/PaymentType.java
@@ -1,6 +1,8 @@
 package app.giftify.shared.domain.event.payment;
 
 // 이거 여기서 정책 검증하면 망한다
+// FIXME
+// 첫번째, 이벤트랑 같이 묶어두면 안되는 enum 인데 같은 폴더에 묶어 놓음
 public enum PaymentType {
 	FUNDING,    // 펀딩 참여 결제
 	CHARGE,
