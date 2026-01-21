6b040b441b3bc29d88b3d8145a53b1678571e1c3
youngjuKim <kjuzoojuk@naver.com>
2026-01-21 17:10:53 +0900

    test(member:wishlist): wishlist(+test)

---
+++ bc/member/src/test/java/app/giftify/wishlist/WishlistTestApplication.java
@@ -0,0 +1,11 @@
+package app.giftify.wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WishlistTestApplication.class, args);
+    }
+}
