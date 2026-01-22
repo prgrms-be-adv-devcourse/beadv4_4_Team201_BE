c0c643e8f05339d638e91a845cc4bc8e54314936
youngjuKim <kjuzoojuk@naver.com>
2026-01-22 14:31:07 +0900

    feat(bc:shared): MoneyVO에 최소 금액 검증 예외 추가

--- bc/shared/src/main/java/app/giftify/shared/domain/vo/Money.java
+++ bc/shared/src/main/java/app/giftify/shared/domain/vo/Money.java
@@ -13,8 +13,11 @@ public record Money(BigDecimal amount) {
 			throw new IllegalArgumentException("금액은 null일 수 없습니다.");
 		}
 
-		if (amount.signum() < 1000) {
-			throw new IllegalArgumentException("펀딩 참여 최소 금액은 1000원 입니다.");
+		if (amount.signum() < 0) {
+			if (amount.compareTo(BigDecimal.valueOf(1000)) < 0) {
+				throw new IllegalArgumentException("펀딩 참여 최소 금액은 1000원 입니다.");
+			}
+			throw new IllegalArgumentException("금액은 음수가 될 수 없습니다.");
 		}
 	}
 
