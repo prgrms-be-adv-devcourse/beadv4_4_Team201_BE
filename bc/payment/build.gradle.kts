43d60daaaf7d154dfac586464d99b914d1ac1baa
chan99k <kjkj5868@gmail.com>
2026-01-27 19:53:43 +0900

    chore: 결제 도메인 의존성 추가 및 테스트 의존성 업데이트
    - `bc:shared` 외 `support:security` 모듈 의존성 추가
    - Spring Validation 및 Security Test 관련 라이브러리 추가

--- bc/payment/build.gradle.kts
+++ bc/payment/build.gradle.kts
@@ -12,8 +12,10 @@ repositories {
 
 dependencies {
     implementation(project(":bc:shared"))
+    implementation(project(":support:security"))
 
     implementation(libs.spring.boot.starter.web)
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.modulith:spring-modulith-starter-core")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
@@ -25,6 +27,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.springframework.modulith:spring-modulith-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
     testImplementation("com.tngtech.archunit:archunit-junit5:1.3.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
