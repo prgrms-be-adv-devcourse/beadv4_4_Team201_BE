c65a98448bb293356d327fa00f9b172a4a8ca8db
Suyeoneee <101303761+suyeon1717@users.noreply.github.com>
2026-01-29 10:01:15 +0900

    Product 도메인 StringUtils 리팩토링 (#160)
    ## Summary
    
    > 이 `PR`에서 `제공`하는 `솔루션`과 그 `설계 의도`를 설명해 주세요. _( like 흑백요리사 )_
    > `변경사항`이 필요한 이유, 즉 `컨텍스트` 와 해결하려는 `문제의 핵심`을 `요약`하고, `리뷰어`가 `어떤 관점`에서
    봐줬으면 좋겠는지 명시해주세요.
    
    
    ---
    
    ## What's Changed
    
    > `PR`에서 `변경되는 점`들을 설명해 주세요. `UI 변경`이 있다면 `스크린샷`을 첨부해주세요.
    
    
    ---
    
    ## Decisions & Trade-offs
    
    > 개발중에 추가로 고려한 부분에 대해서 공유해주세요. 예를 들어,<br>
    > `A 방식 대신 B 방식을 선택한 이유`나, `성능` 혹은 `사용자 경험`을 위해 고민한 흔적을 남겨주세요.
    
    
    ---
    
    ## Future Improvements
    
    > 이번 `PR`의 `범위`에는 포함되지 않았지만, 추후 `고도화`하거나 `보완`하고 싶은 점이 있다면 적어주세요.
    > 예를 들어, 남겨둔 `기술 부채`나 `최적화` 계획, 혹은 `확장성`을 위해 다음 단계에서 `시도해 볼 만한 아이디어`를
    공유해주세요.
    
    
    ---
    
    ### Linked Issues
    
    - closes #

--- bc/catalog/src/main/java/app/giftify/product/domain/Product.java
+++ bc/catalog/src/main/java/app/giftify/product/domain/Product.java
@@ -1,183 +1,182 @@
 package app.giftify.product.domain;
 
-import static app.giftify.product.domain.ProductStatus.*;
-import static app.giftify.product.domain.exception.ProductErrorCode.*;
-
-import java.time.LocalDateTime;
-
 import app.giftify.product.domain.exception.ProductException;
 import app.giftify.shared.domain.event.product.ProductReplicaCreationRequestedEvent;
 import app.giftify.shared.domain.event.product.ProductSaleDisabledEvent;
 import app.giftify.shared.domain.event.product.ProductSaleEnabledEvent;
 import app.giftify.support.jpa.BaseJpaEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDateTime;
+
+import static app.giftify.product.domain.ProductStatus.*;
+import static app.giftify.product.domain.exception.ProductErrorCode.*;
 
 @Entity
 @Table(name = "PRODUCT")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product extends BaseJpaEntity {
-	private Long sellerId;
-	private String sellerNickname;
-	private String name;
-	private String description;
-	private int price;
-	private int stock;
-	private ProductStatus status;
+    private Long sellerId;
+    private String sellerNickname;
+    private String name;
+    private String description;
+    private int price;
+    private int stock;
+    private ProductStatus status;
 
-	public Product(Long sellerId, String sellerNickname, String name, String description, int price, int stock) {
-		validateCreation(sellerId, sellerNickname, name, description, price, stock);
+    public Product(Long sellerId, String sellerNickname, String name, String description, int price, int stock) {
+        validateCreation(sellerId, sellerNickname, name, description, price, stock);
 
-		this.sellerId = sellerId;
-		this.sellerNickname = sellerNickname;
-		this.name = name;
-		this.description = description;
-		this.price = price;
-		this.stock = stock;
-		this.status = DRAFT;
-	}
+        this.sellerId = sellerId;
+        this.sellerNickname = sellerNickname;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.status = DRAFT;
+    }
 
-	// 상품 생성 검증 (Fail-Fast)
-	private void validateCreation(
-		Long sellerId, String sellerNickname, String name, String description, int price, int stock
-	) {
-		if (sellerId == null)
-			throw new ProductException(PRODUCT_SELLER_REQUIRED);
-		if (sellerNickname == null || sellerNickname.isBlank())
-			throw new ProductException(PRODUCT_SELLER_REQUIRED);
-		if (name == null || name.isBlank())
-			throw new ProductException(INVALID_PRODUCT_NAME);
-		if (description == null || description.isBlank())
-			throw new ProductException(INVALID_PRODUCT_DESCRIPTION);
-		if (price <= 0)
-			throw new ProductException(INVALID_PRODUCT_PRICE);
-		if (stock < 0)
-			throw new ProductException(INVALID_PRODUCT_STOCK);
-	}
+    // 상품 생성 검증 (Fail-Fast)
+    private void validateCreation(
+            Long sellerId, String sellerNickname, String name, String description, int price, int stock
+    ) {
+        if (sellerId == null)
+            throw new ProductException(PRODUCT_SELLER_REQUIRED);
+        if (!StringUtils.hasText(sellerNickname))
+            throw new ProductException(PRODUCT_SELLER_REQUIRED);
+        if (!StringUtils.hasText(name))
+            throw new ProductException(INVALID_PRODUCT_NAME);
+        if (!StringUtils.hasText(description))
+            throw new ProductException(INVALID_PRODUCT_DESCRIPTION);
+        if (price <= 0)
+            throw new ProductException(INVALID_PRODUCT_PRICE);
+        if (stock < 0)
+            throw new ProductException(INVALID_PRODUCT_STOCK);
+    }
 
-	/**
-	 * 상품 상태 변경
-	 * todo 상품 상태 변경 이력 컬렉션
-	 */
-	// 상품 등록 승인
-	public void approve() {
-		transitionTo(INACTIVE); // 판매 대기 상태로
-		registerEvent(new ProductReplicaCreationRequestedEvent(
-			LocalDateTime.now(),
-			this.getId(),
-			this.getName(),
-			this.getPrice(),
-			this.getSellerNickname()
-		));
-	}
+    /**
+     * 상품 상태 변경
+     * todo 상품 상태 변경 이력 컬렉션
+     */
+    // 상품 등록 승인
+    public void approve() {
+        transitionTo(INACTIVE); // 판매 대기 상태로
+        registerEvent(new ProductReplicaCreationRequestedEvent(
+                LocalDateTime.now(),
+                this.getId(),
+                this.getName(),
+                this.getPrice(),
+                this.getSellerNickname()
+        ));
+    }
 
-	// 상품 등록 거절
-	public void reject() {
-		transitionTo(REJECTED);
-	}
+    // 상품 등록 거절
+    public void reject() {
+        transitionTo(REJECTED);
+    }
 
-	// 상품 판매 시작
-	public void active() {
-		transitionTo(ACTIVE);
-		registerEvent(new ProductSaleEnabledEvent(LocalDateTime.now(), this.getId())); // 판매 가능 이벤트 발생
-	}
+    // 상품 판매 시작
+    public void active() {
+        transitionTo(ACTIVE);
+        registerEvent(new ProductSaleEnabledEvent(LocalDateTime.now(), this.getId())); // 판매 가능 이벤트 발생
+    }
 
-	// 상품 판매 중지
-	public void inActive() {
-		transitionTo(INACTIVE);
-		registerEvent(new ProductSaleDisabledEvent(
-			LocalDateTime.now(), this.getId()
-		)); // 판매 불가능 이벤트 발생
-	}
+    // 상품 판매 중지
+    public void inActive() {
+        transitionTo(INACTIVE);
+        registerEvent(new ProductSaleDisabledEvent(
+                LocalDateTime.now(), this.getId()
+        )); // 판매 불가능 이벤트 발생
+    }
 
-	// 상품 상태 검증 todo Map<from,to> 상태 머신
-	private void validateTransition(ProductStatus toStatus) {
-		if (toStatus == ProductStatus.DRAFT) {
-			throw new ProductException(PRODUCT_CANNOT_CHANGE_STATUS_TO_DRAFT);
-		}
-		switch (toStatus) {
-			case REJECTED -> {
-				if (this.status != DRAFT)
-					throw new ProductException(PRODUCT_NOT_IN_DRAFT_STATUS);
-			}
-			case ACTIVE -> {
-				if (this.status != INACTIVE)
-					throw new ProductException(PRODUCT_NOT_IN_INACTIVE_STATUS);
-			}
-			case INACTIVE -> {
-				if (this.status != DRAFT && this.status != ACTIVE)
-					throw new ProductException(PRODUCT_REJECTED_CANNOT_BE_ACTIVATED);
-			}
-		}
-	}
+    // 상품 상태 검증 todo Map<from,to> 상태 머신
+    private void validateTransition(ProductStatus toStatus) {
+        if (toStatus == ProductStatus.DRAFT) {
+            throw new ProductException(PRODUCT_CANNOT_CHANGE_STATUS_TO_DRAFT);
+        }
+        switch (toStatus) {
+            case REJECTED -> {
+                if (this.status != DRAFT)
+                    throw new ProductException(PRODUCT_NOT_IN_DRAFT_STATUS);
+            }
+            case ACTIVE -> {
+                if (this.status != INACTIVE)
+                    throw new ProductException(PRODUCT_NOT_IN_INACTIVE_STATUS);
+            }
+            case INACTIVE -> {
+                if (this.status != DRAFT && this.status != ACTIVE)
+                    throw new ProductException(PRODUCT_REJECTED_CANNOT_BE_ACTIVATED);
+            }
+        }
+    }
 
-	// 상태 변경
-	private void transitionTo(ProductStatus toStatus) {
-		validateTransition(toStatus);
-		this.status = toStatus;
-	}
+    // 상태 변경
+    private void transitionTo(ProductStatus toStatus) {
+        validateTransition(toStatus);
+        this.status = toStatus;
+    }
 
-	/**
-	 * 상품 정보 수정
-	 */
-	public void updateName(String newName) {
-		this.name = newName;
-	}
+    /**
+     * 상품 정보 수정
+     */
+    public void updateName(String newName) {
+        this.name = newName;
+    }
 
-	public void updateDescription(String newDescription) {
-		this.description = newDescription;
-	}
+    public void updateDescription(String newDescription) {
+        this.description = newDescription;
+    }
 
-	public void updatePrice(int newPrice) {
-		this.price = newPrice;
-	}
+    public void updatePrice(int newPrice) {
+        this.price = newPrice;
+    }
 
-	// 상품 재고 수정 (판매자 수동)
-	public StockChangeResult updateStock(int newStock) {
-		int beforeStock = this.stock;
-		this.stock = newStock;
+    // 상품 재고 수정 (판매자 수동)
+    public StockChangeResult updateStock(int newStock) {
+        int beforeStock = this.stock;
+        this.stock = newStock;
 
-		if (beforeStock > 0 && newStock == 0 && this.status == ACTIVE) {
-			registerEvent(new ProductSaleDisabledEvent(LocalDateTime.now(), this.getId())); // 품절 이벤트 발생
-		}
-		if (beforeStock == 0 && newStock > 0 && this.status == ACTIVE) {
-			registerEvent(new ProductSaleEnabledEvent(LocalDateTime.now(), this.getId())); // 판매 가능 이벤트 발생
-		}
+        if (beforeStock > 0 && newStock == 0 && this.status == ACTIVE) {
+            registerEvent(new ProductSaleDisabledEvent(LocalDateTime.now(), this.getId())); // 품절 이벤트 발생
+        }
+        if (beforeStock == 0 && newStock > 0 && this.status == ACTIVE) {
+            registerEvent(new ProductSaleEnabledEvent(LocalDateTime.now(), this.getId())); // 판매 가능 이벤트 발생
+        }
 
-		return new StockChangeResult(beforeStock, this.stock, newStock - beforeStock);
-	}
+        return new StockChangeResult(beforeStock, this.stock, newStock - beforeStock);
+    }
 
-	// 상품 재고 감소
-	public StockChangeResult decreaseStock(int quantity) {
-		if (this.stock < quantity)
-			throw new ProductException(PRODUCT_OUT_OF_STOCK);
+    // 상품 재고 감소
+    public StockChangeResult decreaseStock(int quantity) {
+        if (this.stock < quantity)
+            throw new ProductException(PRODUCT_OUT_OF_STOCK);
 
-		int beforeStock = this.stock;
-		this.stock -= quantity;
+        int beforeStock = this.stock;
+        this.stock -= quantity;
 
-		if (this.stock == 0 && this.status == ACTIVE)
-			registerEvent(new ProductSaleDisabledEvent(LocalDateTime.now(), this.getId())); // 품절 이벤트 발생
+        if (this.stock == 0 && this.status == ACTIVE)
+            registerEvent(new ProductSaleDisabledEvent(LocalDateTime.now(), this.getId())); // 품절 이벤트 발생
 
-		return new StockChangeResult(beforeStock, this.stock, -quantity);
-	}
+        return new StockChangeResult(beforeStock, this.stock, -quantity);
+    }
 
-	// 상품 재고 추가
-	public StockChangeResult increaseStock(int quantity) {
-		int beforeStock = this.stock;
-		this.stock += quantity;
+    // 상품 재고 추가
+    public StockChangeResult increaseStock(int quantity) {
+        int beforeStock = this.stock;
+        this.stock += quantity;
 
-		if (beforeStock == 0 && this.status == ACTIVE)
-			registerEvent(new ProductSaleEnabledEvent(LocalDateTime.now(), this.getId())); // 판매 가능 이벤트 발생
+        if (beforeStock == 0 && this.status == ACTIVE)
+            registerEvent(new ProductSaleEnabledEvent(LocalDateTime.now(), this.getId())); // 판매 가능 이벤트 발생
 
-		return new StockChangeResult(beforeStock, this.stock, quantity);
-	}
+        return new StockChangeResult(beforeStock, this.stock, quantity);
+    }
 
-	public record StockChangeResult(int beforeStock, int afterStock, int delta) {
-	}
+    public record StockChangeResult(int beforeStock, int afterStock, int delta) {
+    }
 }
