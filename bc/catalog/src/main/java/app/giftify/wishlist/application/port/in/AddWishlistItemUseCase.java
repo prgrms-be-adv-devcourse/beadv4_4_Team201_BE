dc291387521830113383b76c596d8c22deabe61c
SUYEON <suyun1886@gmail.com>
2026-01-30 12:31:28 +0900

    refactor: Command 단순화 및 레플리카 관련 코드 정리
    - WishlistProductQueryPort 및 관련 테스트 삭제
    - WishlistItemAddCommand에서 불필요한 status 필드 제거
    - ProductSupport, WishlistSupport 사용하도록 테스트 수정

--- bc/catalog/src/main/java/app/giftify/wishlist/application/port/in/AddWishlistItemUseCase.java
+++ bc/catalog/src/main/java/app/giftify/wishlist/application/port/in/AddWishlistItemUseCase.java
@@ -1,14 +1,12 @@
 package app.giftify.wishlist.application.port.in;
 
 import app.giftify.wishlist.core.domain.WishlistItem;
-import app.giftify.wishlist.core.domain.WishlistItemStatus;
 
 public interface AddWishlistItemUseCase {
-	WishlistItem addWishlistItem(Long memberId, WishlistItemAddCommand command);
+    WishlistItem addWishlistItem(Long memberId, WishlistItemAddCommand command);
 
-	record WishlistItemAddCommand(
-		Long productId,
-		WishlistItemStatus wishListItemStatus
-	) {
-	}
+    record WishlistItemAddCommand(
+            Long productId
+    ) {
+    }
 }
