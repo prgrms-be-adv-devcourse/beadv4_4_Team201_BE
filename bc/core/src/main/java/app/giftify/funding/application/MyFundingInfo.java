9067177a8e84eface16dbc83acdf055accc1f94b
juykoung <juyk.oung@gmail.com>
2026-01-31 10:56:18 +0900

    refactor: funding 디렉토리 헥사고날 구조로 변경 (#173)
    ## Summary
    
    로직 등 코드변경은 아예 없으며 오직 디렉토리 구조만 변경된 pr입니다.
    다른 모듈과 구조가 달라 파일 찾기 힘들 경우를 위해 우선 올립니다!
    
    세세한 디렉토리 변경은 추후 코드 리팩토링과 함께 진행하겠습니다.

---
+++ bc/core/src/main/java/app/giftify/funding/application/MyFundingInfo.java
@@ -0,0 +1,13 @@
+package app.giftify.funding.application;
+
+import app.giftify.funding.adpater.outbound.jpa.Funding;
+
+public record MyFundingInfo(
+        Funding funding,
+        Integer myContribution
+) {
+    // 생성자에서 null 처리
+    public MyFundingInfo(Funding funding, Long myContribution) {
+        this(funding, myContribution != null ? myContribution.intValue() : 0);
+    }
+}
---
+++ bc/core/src/main/java/app/giftify/funding/domain/exception/FundingException.java
@@ -0,0 +1,24 @@
+package app.giftify.funding.domain.exception;
+
+import app.giftify.shared.api.exception.DomainException;
+import app.giftify.shared.api.exception.ErrorCode;
+
+public class FundingException extends DomainException {
+
+    public FundingException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public FundingException(ErrorCode errorCode, String message) {
+        super(errorCode, message);
+    }
+
+    /**
+     * ErrorCode의 메시지를 포맷팅하여 예외 생성
+     * @param errorCode 에러 코드
+     * @param args 포맷 인자 (String.format 사용)
+     */
+    public FundingException(ErrorCode errorCode, Object... args) {
+        super(errorCode, String.format(errorCode.getMessage(), args));
+    }
+}
