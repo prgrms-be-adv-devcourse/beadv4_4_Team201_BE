1357468eda2db25f06bf0139acecba79fafedebf
ju <juyk.oung@gmail.com>
2026-02-02 14:36:13 +0900

    remove: 불필요 파일 삭제

--- bc/core/src/main/java/app/giftify/funding/application/FundingFromOrderCommand.java
+++
@@ -1,9 +0,0 @@
-package app.giftify.funding.application;
-
-public record FundingFromOrderCommand(
-        Long wishlistItemId,
-        Long participantId,
-        Long receiverId,
-        Integer amount
-) {
-}
