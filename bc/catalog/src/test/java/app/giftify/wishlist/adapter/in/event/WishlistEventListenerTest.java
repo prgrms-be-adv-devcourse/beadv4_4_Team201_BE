67d81c02d0b700db7327455d9d597b74c6e3a2cb
juykoung <juyk.oung@gmail.com>
2026-02-05 16:14:23 +0900

    refactor: 펀딩수락이벤트에 상품ID 추가 (#204)
    ## Summary
    
    상품 재고 차감을 위해 `펀딩수락이벤트`에 `상품ID` 추가

--- bc/catalog/src/test/java/app/giftify/wishlist/adapter/in/event/WishlistEventListenerTest.java
+++ bc/catalog/src/test/java/app/giftify/wishlist/adapter/in/event/WishlistEventListenerTest.java
@@ -130,7 +130,8 @@ class WishlistEventListenerTest {
         void handleFundingAccepted_ChangesStatusToCompleted() {
             // given
             Long wishlistItemId = 1L;
-            FundingAcceptedEvent event = new FundingAcceptedEvent(100L, wishlistItemId, LocalDateTime.now());
+            Long productId = 1L;
+            FundingAcceptedEvent event = new FundingAcceptedEvent(100L, wishlistItemId, productId, LocalDateTime.now());
 
             WishlistItem wishlistItem = WishlistItem.builder()
                     .id(wishlistItemId)
