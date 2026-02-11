dd1084871803beb0ae29353607a1ee736a5b8d19
ju <juyk.oung@gmail.com>
2026-02-11 12:22:25 +0900

    remove: 사용 안하는 FundingMember 관련 파일 삭제

--- bc/core/src/main/java/app/giftify/funding/adpater/outbound/repository/FundingMemberRepository.java
+++
@@ -1,8 +0,0 @@
-package app.giftify.funding.adpater.outbound.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.giftify.funding.adpater.outbound.jpa.FundingMember;
-
-public interface FundingMemberRepository extends JpaRepository<FundingMember, Long> {
-}
