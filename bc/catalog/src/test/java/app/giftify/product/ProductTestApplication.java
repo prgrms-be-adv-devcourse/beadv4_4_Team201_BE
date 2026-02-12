bd4065d0b47de54021ea01510465a4d87f8451da
SUYEON <suyun1886@gmail.com>
2026-02-12 11:58:50 +0900

    test: ProductEsRepository Testcontainers 테스트 추가

--- bc/catalog/src/test/java/app/giftify/product/ProductTestApplication.java
+++ bc/catalog/src/test/java/app/giftify/product/ProductTestApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -25,6 +26,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         "app.giftify.replica",
         "app.giftify.support.jpa"
 })
+@EnableElasticsearchRepositories(basePackages = "app.giftify.product")
 @EntityScan(basePackages = {
         "app.giftify.product",
         "app.giftify.replica",
---
+++ bc/catalog/src/test/java/app/giftify/product/TestContainersConfiguration.java
@@ -0,0 +1,17 @@
+package app.giftify.product;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestContainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    ElasticsearchContainer elasticsearchContainer() {
+        return new ElasticsearchContainer(DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:7.17.10"));
+    }
+}
