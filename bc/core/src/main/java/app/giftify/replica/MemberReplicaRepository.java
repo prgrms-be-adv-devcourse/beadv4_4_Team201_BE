979e8d6ac83013eb07d4839c5704ccedffeb598e
juyeongkyeong <juyk.oung@gmail.com>
2026-02-18 00:52:12 +0900

    test: 닉네임 관련 test 코드 반영

--- bc/core/src/main/java/app/giftify/replica/MemberReplicaRepository.java
+++ bc/core/src/main/java/app/giftify/replica/MemberReplicaRepository.java
@@ -2,8 +2,6 @@ package app.giftify.replica;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MemberReplicaRepository extends JpaRepository<MemberReplica, Long> {
-    Optional<MemberReplica> findById(Long id);
+
 }
