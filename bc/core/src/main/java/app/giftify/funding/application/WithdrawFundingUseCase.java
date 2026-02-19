b1668aea05999c75cee901aeb04b7a2d783b1dfa
ju <juyk.oung@gmail.com>
2026-02-19 15:53:44 +0900

    feat: 주문 단건 취소 시, 펀딩에서 기여금 및 기여자 삭제 구현

---
+++ bc/core/src/main/java/app/giftify/funding/application/WithdrawFundingUseCase.java
@@ -0,0 +1,31 @@
+package app.giftify.funding.application;
+
+import app.giftify.funding.adpater.outbound.jpa.Funding;
+import app.giftify.funding.adpater.outbound.repository.FundingParticipantMemberRepository;
+import app.giftify.funding.adpater.outbound.repository.FundingRepository;
+import app.giftify.funding.domain.FundingStatus;
+import app.giftify.funding.domain.exception.FundingErrorCode;
+import app.giftify.funding.domain.exception.FundingException;
+import app.giftify.shared.domain.vo.Money;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class WithdrawFundingUseCase {
+    private final FundingRepository fundingRepository;
+    private final FundingParticipantMemberRepository fundingParticipantMemberRepository;
+
+    @Transactional
+    public void withdrawFunding(Long fundingId, Long participantId, Money amount) {
+        // 펀딩이 만료/수락/거절/마감 상태가 아니여야 함 -> 즉 진행중이거나 달성 상태일 때만 가능 -> 도메인 내 메서드에서 처리
+        Funding funding = fundingRepository.findById(fundingId).
+                orElseThrow(() -> new FundingException(FundingErrorCode.FUNDING_NOT_FOUND, fundingId));
+
+        // Money 객체에서 Integer 값 추출하여 withdraw 메서드에 전달
+        funding.withdraw(amount.toBigDecimalValue().intValue());
+        fundingParticipantMemberRepository.deleteByFundingIdAndParticipantId(fundingId, participantId);
+
+    }
+}
