cd27d7579733fca36a0af209950adfd00020bbea
chan99k <kjkj5868@gmail.com>
2026-02-20 10:49:41 +0900

    feat: 알림 기능을 위한 도메인 모델 작

---
+++ bc/notification/src/main/java/app/giftify/notification/domain/NotificationType.java
@@ -0,0 +1,22 @@
+package app.giftify.notification.domain;
+
+public enum NotificationType {
+	FUNDING_CREATED("새로운 펀딩이 시작되었습니다"),
+	FUNDING_ACHIEVED("펀딩이 완료되었습니다"),
+	FUNDING_EXPIRED("펀딩이 만료되었습니다"),
+	FUNDING_CANCELED("펀딩이 취소되었습니다"),
+	FRIEND_REQUEST_RECEIVED("친구 요청이 도착했습니다"),
+	FRIEND_REQUEST_ACCEPTED("친구 요청이 수락되었습니다"),
+	PAYMENT_SUCCEEDED("결제가 완료되었습니다"),
+	PAYMENT_FAILED("결제에 실패했습니다"),
+	PAYMENT_CANCEL_SUCCEEDED("결제 취소가 완료되었습니다"),
+	PAYMENT_CANCEL_FAILED("결제 취소에 실패했습니다");
+
+	private final String title;
+
+	NotificationType(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() { return title; }
+}
