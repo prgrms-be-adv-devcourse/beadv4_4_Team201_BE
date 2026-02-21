ea21f03012cc211e3664971fa6f4ff340ac0b59a
juyeongkyeong <juyk.oung@gmail.com>
2026-02-21 15:44:36 +0900

    refactor: withdrawFunding -> withdrawByWishlistItem 메서드명 변경

--- bc/core/src/main/java/app/giftify/funding/application/WithdrawFundingUseCase.java
+++ bc/core/src/main/java/app/giftify/funding/application/WithdrawFundingUseCase.java
@@ -17,7 +17,7 @@ public class WithdrawFundingUseCase {
     private final FundingParticipantMemberRepository fundingParticipantMemberRepository;
 
     @Transactional
-    public void withdrawFunding(Long wishlistItemId, Long participantId, Money amount) {
+    public void withdrawByWishlistItem (Long wishlistItemId, Long participantId, Money amount) {
         // 진행중이거나 달성 상태일 때만 가능 -> 도메인 내 메서드에서 처리
         Funding funding = fundingRepository.findActiveByWishlistItemId(wishlistItemId).
                 orElseThrow(() -> new FundingException(FundingErrorCode.FUNDING_NOT_FOUND, wishlistItemId));
