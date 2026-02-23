5173f95bc24f2d474157cb960120725a21fd6012
juykoung <juyk.oung@gmail.com>
2026-02-23 18:09:30 +0900

    주문 취소 -> 펀딩 기여 철회 구현 (#300)
    ## Summary
    
    > 이 `PR`에서 `제공`하는 `솔루션`과 그 `설계 의도`를 설명해 주세요. _( like 흑백요리사 )_
    > `변경사항`이 필요한 이유, 즉 `컨텍스트` 와 해결하려는 `문제의 핵심`을 `요약`하고, `리뷰어`가 `어떤 관점`에서
    봐줬으면 좋겠는지 명시해주세요.
    
    
    `OrderCanceledEvent`를 받아 해당 기여자와 기여금을 펀딩에서 제거하는 기능 구현

---
+++ bc/core/src/main/java/app/giftify/funding/application/WithdrawFundingUseCase.java
@@ -0,0 +1,28 @@
+package app.giftify.funding.application;
+
+import app.giftify.funding.adpater.outbound.jpa.Funding;
+import app.giftify.funding.adpater.outbound.repository.FundingParticipantMemberRepository;
+import app.giftify.funding.adpater.outbound.repository.FundingRepository;
+import app.giftify.funding.domain.exception.FundingErrorCode;
+import app.giftify.funding.domain.exception.FundingException;
+import app.giftify.shared.domain.vo.Money;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class WithdrawFundingUseCase {
+    private final FundingRepository fundingRepository;
+    private final FundingParticipantMemberRepository fundingParticipantMemberRepository;
+
+    @Transactional
+    public void withdrawByWishlistItem (Long wishlistItemId, Long participantId, Money amount) {
+        // 진행중이거나 달성 상태일 때만 가능 -> 도메인 내 메서드에서 처리
+        Funding funding = fundingRepository.findActiveByWishlistItemId(wishlistItemId).
+                orElseThrow(() -> new FundingException(FundingErrorCode.FUNDING_NOT_FOUND, wishlistItemId));
+
+        funding.withdraw(amount.toBigDecimalValue().intValue());
+        fundingParticipantMemberRepository.deleteByFundingIdAndParticipantId(funding.getId(), participantId);
+    }
+}
