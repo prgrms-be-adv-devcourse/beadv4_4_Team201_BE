195747fa6ebe40df9ecfd8298e9d197286a8148f
ju <juyk.oung@gmail.com>
2026-02-23 09:41:17 +0900

    refactor: OrderCanceledEventListener 내 items -> getter 이용으로 변경

--- bc/core/src/main/java/app/giftify/funding/adpater/inbound/OrderCanceledEventListener.java
+++ bc/core/src/main/java/app/giftify/funding/adpater/inbound/OrderCanceledEventListener.java
@@ -3,6 +3,7 @@ package app.giftify.funding.adpater.inbound;
 import app.giftify.funding.application.WithdrawFundingUseCase;
 import app.giftify.funding.domain.exception.FundingErrorCode;
 import app.giftify.funding.domain.exception.FundingException;
+import app.giftify.shared.domain.event.order.OrderCanceledEvent;
 import app.giftify.shared.domain.type.TargetType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,22 +19,22 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class OrderCanceledEventListener {
     private final WithdrawFundingUseCase withdrawFundingUseCase;
 
-//    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-//    @Transactional(propagation = Propagation.REQUIRES_NEW)
-//    public void handle(OrderCanceledEvent event) {
-//        event.items().stream()
-//                .filter(item -> item.targetType() == TargetType.FUNDING)
-//                .forEach(item -> {
-//                    log.info(
-//                            "[Funding] 주문 취소 -> 펀딩 기여 철회. orderId={}, orderItemId={}, buyerId={}, targetId={}",
-//                            event.orderId(), item.orderItemId(), item.buyerId(), item.targetId()
-//                    );
-//
-//                    withdrawFundingUseCase.withdrawByWishlistItem(
-//                            item.targetId(),      // wishlistItemId
-//                            item.buyerId(),
-//                            item.cancelAmount()
-//                    );
-//                });
-//    }
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void handle(OrderCanceledEvent event) {
+        event.getItems().stream()
+                .filter(item -> item.targetType() == TargetType.FUNDING)
+                .forEach(item -> {
+                    log.info(
+                            "[Funding] 주문 취소 -> 펀딩 기여 철회. orderId={}, orderItemId={}, buyerId={}, targetId={}",
+                            event.getOrderId(), item.orderItemId(), item.buyerId(), item.targetId()
+                    );
+
+                    withdrawFundingUseCase.withdrawByWishlistItem(
+                            item.targetId(),      // wishlistItemId
+                            item.buyerId(),
+                            item.cancelAmount()
+                    );
+                });
+    }
 }
