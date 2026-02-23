61f6ca99463c37d0e5e8892f5991160ba2314f4e
ju <juyk.oung@gmail.com>
2026-02-23 09:30:20 +0900

    chore: 잠시 주석

--- bc/core/src/main/java/app/giftify/funding/adpater/inbound/OrderCanceledEventListener.java
+++ bc/core/src/main/java/app/giftify/funding/adpater/inbound/OrderCanceledEventListener.java
@@ -18,22 +18,22 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class OrderCanceledEventListener {
     private final WithdrawFundingUseCase withdrawFundingUseCase;
 
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void handle(OrderCanceledEvent event) {
-        event.items().stream()
-                .filter(item -> item.targetType() == TargetType.FUNDING)
-                .forEach(item -> {
-                    log.info(
-                            "[Funding] 주문 취소 -> 펀딩 기여 철회. orderId={}, orderItemId={}, buyerId={}, targetId={}",
-                            event.orderId(), item.orderItemId(), item.buyerId(), item.targetId()
-                    );
-
-                    withdrawFundingUseCase.withdrawByWishlistItem(
-                            item.targetId(),      // wishlistItemId
-                            item.buyerId(),
-                            item.cancelAmount()
-                    );
-                });
-    }
+//    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+//    @Transactional(propagation = Propagation.REQUIRES_NEW)
+//    public void handle(OrderCanceledEvent event) {
+//        event.items().stream()
+//                .filter(item -> item.targetType() == TargetType.FUNDING)
+//                .forEach(item -> {
+//                    log.info(
+//                            "[Funding] 주문 취소 -> 펀딩 기여 철회. orderId={}, orderItemId={}, buyerId={}, targetId={}",
+//                            event.orderId(), item.orderItemId(), item.buyerId(), item.targetId()
+//                    );
+//
+//                    withdrawFundingUseCase.withdrawByWishlistItem(
+//                            item.targetId(),      // wishlistItemId
+//                            item.buyerId(),
+//                            item.cancelAmount()
+//                    );
+//                });
+//    }
 }
