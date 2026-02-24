fba825f00389b039ad67f5570d2d61e3c880a81f
ju <juyk.oung@gmail.com>
2026-02-24 12:03:38 +0900

    chore: `data-local.sql`, flyway 내 펀딩 - 상품이름 추가

--- bootstrap/api-server/src/main/resources/data-local.sql
+++ bootstrap/api-server/src/main/resources/data-local.sql
@@ -333,7 +333,7 @@ ALTER TABLE cart_item
 -- -----------------------------------------------------------------------------
 -- 10. FUNDING (펀딩) — V1.2.4
 -- -----------------------------------------------------------------------------
-INSERT INTO funding (id, version, wishlist_item_id, product_id, receiver_id, target_amount, current_amount,
+INSERT INTO funding (id, version, wishlist_item_id, product_id, productName, receiver_id, target_amount, current_amount,
                      status, deadline, achieved_at, closed_at,
                      created_at, updated_at, created_by, updated_by)
 VALUES
@@ -341,15 +341,15 @@ VALUES
      '2026-03-01 23:59:59', NULL, NULL,
      NOW(), NOW(), 'SYSTEM', 'SYSTEM'),
 
-    (2, 0,9, 1, 5, 359000, 359000, 'ACHIEVED',
+    (2, 0,9, 51, '샤넬 가브리엘 향수', 5, 359000, 359000, 'ACHIEVED',
      '2026-02-28 23:59:59', '2026-02-07 14:30:00', NULL,
      NOW(), NOW(), 'SYSTEM', 'SYSTEM'),
 
-    (3, 0,4, 3, 2, 415000, 120000, 'EXPIRED',
+    (3, 0,4, 53, '에스티로더 갈색병',2, 415000, 120000, 'EXPIRED',
      '2026-02-01 23:59:59', NULL, '2026-02-01 23:59:59',
      NOW(), NOW(), 'SYSTEM', 'SYSTEM'),
 
-    (4, 0,2, 3, 2, 415000, 415000, 'ACHIEVED',
+    (4, 0,2, 53, '에스티로더 갈색병',2, 415000, 415000, 'ACHIEVED',
      '2026-02-15 23:59:59', '2026-02-13 23:59:59', NULL,
      NOW(), NOW(), 'SYSTEM', 'SYSTEM');
 
