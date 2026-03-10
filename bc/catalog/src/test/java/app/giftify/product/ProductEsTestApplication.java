12a09d72ccede0f833a9326725b5dca4aaeb1b90
chan99k <kjkj5868@gmail.com>
2026-03-10 11:18:37 +0900

    refactor: Boot 4 패키지 이전 및 Hibernate 7 엔티티 수정
      Boot 4 패키지 변경:
      - EntityScan: boot.autoconfigure.domain -> boot.persistence.autoconfigure
      - SecurityAutoConfiguration: excludeName 방식으로 변경
    
      Hibernate 7 strict @Id 검증 대응:
      - BaseJpaEntity 상속 시 중복 @Id 필드 제거
      - constructor-level @Builder + super(id) 패턴으로 전환

--- bc/catalog/src/test/java/app/giftify/product/ProductEsTestApplication.java
+++ bc/catalog/src/test/java/app/giftify/product/ProductEsTestApplication.java
@@ -3,8 +3,7 @@ package app.giftify.product;
 import app.giftify.security.common.config.SharedSecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
@@ -13,8 +12,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(
         exclude = {
-                SecurityAutoConfiguration.class,
                 SharedSecurityAutoConfiguration.class
+        },
+        excludeName = {
+                "org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration"
         }
 )
 @ComponentScan(
