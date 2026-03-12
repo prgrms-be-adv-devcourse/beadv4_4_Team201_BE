c7ce090f071d7019b448189aab9c463cc4bdbae0
SUYEON <suyun1886@gmail.com>
2026-03-13 02:05:56 +0900

    feat: 펀딩/일반 주문 재고 차감을 decreaseStockByOrder로 통합
    - OrderConfirmPendingEvent 기반으로 상품별 수량을 받아 재고를 차감
    - TreeMap으로 productId 오름차순 정렬하여 데드락을 방지하고, 트랜잭션 단위로 원자성을 보장

--- bc/catalog/src/main/java/app/giftify/product/application/port/in/DecreaseProductStockUseCase.java
+++ bc/catalog/src/main/java/app/giftify/product/application/port/in/DecreaseProductStockUseCase.java
@@ -1,8 +1,11 @@
 package app.giftify.product.application.port.in;
 
+import java.util.Map;
+
 public interface DecreaseProductStockUseCase {
     // 펀딩에 의한 재고 감소
-    void decreaseStockByFunding(Long productId);
+    // void decreaseStockByFunding(Long productId);
 
-    // todo 일반 주문에 의한 재고 감소
+    // 주문에 의한 재고 감소 (펀딩/일반 주문 통합)
+    void decreaseStockByOrder(Map<Long, Long> productQuantityMap);
 }
