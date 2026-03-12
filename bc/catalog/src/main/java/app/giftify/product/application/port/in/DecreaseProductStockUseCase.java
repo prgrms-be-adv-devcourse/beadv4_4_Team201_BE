8fbbe76d7047f0c0b564ed6439357b2dd67d8eb1
SUYEON <suyun1886@gmail.com>
2026-03-13 03:05:21 +0900

    command: 주석 제거

--- bc/catalog/src/main/java/app/giftify/product/application/port/in/DecreaseProductStockUseCase.java
+++ bc/catalog/src/main/java/app/giftify/product/application/port/in/DecreaseProductStockUseCase.java
@@ -3,9 +3,6 @@ package app.giftify.product.application.port.in;
 import java.util.Map;
 
 public interface DecreaseProductStockUseCase {
-    // 펀딩에 의한 재고 감소
-    // void decreaseStockByFunding(Long productId);
-
     // 주문에 의한 재고 감소 (펀딩/일반 주문 통합)
     void decreaseStockByOrder(Map<Long, Long> productQuantityMap);
 }
