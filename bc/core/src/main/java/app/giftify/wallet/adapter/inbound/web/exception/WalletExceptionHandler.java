60bc3a255dd9760dc8d7903eedff27771512acc3
chan99k <kjkj5868@gmail.com>
2026-03-29 20:09:28 +0900

    refactor: PaymentExceptionHandler BC 경계 분리 및 CoreFacade 미사용 메서드 제거

---
+++ bc/core/src/main/java/app/giftify/wallet/adapter/inbound/web/exception/WalletExceptionHandler.java
@@ -0,0 +1,18 @@
+package app.giftify.wallet.adapter.inbound.web.exception;
+
+import app.giftify.shared.api.response.ErrorResponse;
+import app.giftify.wallet.domain.WalletException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackages = "app.giftify.wallet")
+public class WalletExceptionHandler {
+
+    @ExceptionHandler(WalletException.class)
+    public ResponseEntity<ErrorResponse> handleWalletException(WalletException e) {
+        return ResponseEntity
+                .status(e.getErrorCode().getStatusCode())
+                .body(new ErrorResponse(e.getErrorCode().getCode(), e.getMessage()));
+    }
+}
